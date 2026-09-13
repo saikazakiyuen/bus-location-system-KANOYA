dc16dea2d45811f8a0c28e1ba0279196ac330925
Minato_IZ <saikazakiyuen@gmail.com>
2026-09-14 01:51:23 +0900

    Create config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+const systemConfig = {
+  routes: [
+    { id: "101A", name: "系統101A 垂水〜志布志 (市役所前 経由)", color: "#00AAFF" },
+    { id: "101B", name: "系統101B 垂水〜志布志 (市役所前 非経由)", color: "#0055AA" },
+    { id: "102",  name: "系統102 鹿屋〜高山・内之浦", color: "#FF6600" },
+    { id: "103",  name: "系統103 鹿屋・根占〜佐多", color: "#00AA00" }
+  ]
+};
