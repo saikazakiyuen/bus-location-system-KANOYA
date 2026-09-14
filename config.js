63da88311b118893da70e8aad662a0c6e2322fcb
Minato_IZ <saikazakiyuen@gmail.com>
2026-09-14 11:08:35 +0900

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ const systemConfig = {
     { id: "101A", name: "系統101A 垂水〜志布志 (市役所前 経由)", color: "#00AAFF" },
     { id: "101B", name: "系統101B 垂水〜志布志 (市役所前 非経由)", color: "#0055AA" },
     { id: "102A", name: "系統102A 鹿屋〜高山・内之浦 (論地橋 経由)", color: "#FF6600" },
-    { id: "102B", name: "系統102B 鹿屋〜高山・内之浦 (吾平 経由)", color: "#FF00AA" }
+    { id: "102B", name: "系統102B 鹿屋〜高山・内之浦 (吾平 経由)", color: "#FF00AA" },
     { id: "103", name: "系統103 佐多方面", color: "#FF00AA" }
 
   ]
